c28849e0434f87c7d18272588e4f884625c0b26d
Shawn <your-smcmurphy@gmail.com>
2026-01-04 12:31:06 -0800

    Force update vite config with base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+//update for deployment//
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -5,6 +7,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // ADD THIS LINE BELOW
+      base: '/Crescent_Hill_Nursery/', 
       server: {
         port: 3000,
         host: '0.0.0.0',
